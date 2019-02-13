b8dfa40329579a8919c333b075cf17c83b840126
Vakindu Philliam <Pvakindu@gmail.com>
2019-02-13 16:16:24 -0600

    Add files via upload

---
+++ Python_Twitter_Streaming.py
@@ -0,0 +1,43 @@
+#Connecting to Twitter Streaming API and downloading data
+#We will be using a Python library called Tweepy to connect to Twitter Streaming API 
+#and downloading the data. If you don't have Tweepy installed in your machine, 
+#go to this link, and follow the installation instructions.
+
+#Next create, a file called twitter_streaming.py, and copy into it the code below. 
+#Make sure to enter your credentials into access_token, access_token_secret, consumer_key, 
+#and consumer_secret.
+
+
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "ENTER YOUR ACCESS TOKEN"
+access_token_secret = "ENTER YOUR ACCESS TOKEN SECRET"
+consumer_key = "ENTER YOUR API KEY"
+consumer_secret = "ENTER YOUR API SECRET"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
